9cf104e2c1e37fc2b3e3182dcead83ce8286fb23
Tacəddin Əsədzadə <tcddnesedzade@fmail.com>
2024-10-03 08:57:39 +0400

    c# code

---
+++ task one/Program.cs
@@ -0,0 +1,15 @@
+﻿int divisors = 0;
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    if (num % i == 0)
+    {
+        divisors++;
+
+    }
+}
+if (divisors > 2)
+{
+    Console.WriteLine("murekkebdir");
+}
+else Console.WriteLine("sadedir");
